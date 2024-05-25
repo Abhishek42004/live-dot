5898024892dc1e92fe2b2849aeac501169f84e7e
Abhishek Agrawal <ashu.agrawalksj@gmail.com>
2024-05-26 04:35:30 +0530

    nice work

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 require('dotenv').config();
 
 // Define the port
-const port = process.env.PORT || 5000; // Default to port 5000 if PORT environment variable is not set
+const port = process.env.PORT || 8000; // Default to port 5000 if PORT environment variable is not set
 
 // Create the server
 const server = http.createServer((req, res) => {
